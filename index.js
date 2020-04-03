620dcd5b1a21c6c7f97e609fbc65d3db979310d0
dlinkoln <dlinkoln99@gmail.com>
2020-04-03 18:43:45 +0300

    new branch

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 import Figure from "./figs/figure.js"
 
 function makeBoard() {
-  var container=document.getElementById("app")
+    var container = document.getElementById("app")
 
-  var wSize = Math.min(window.innerHeight, window.innerWidth)
-  container.style.width = wSize/2 + "px"
-  container.style.height = wSize/2 + "px"
+    var wSize = Math.min(window.innerHeight, window.innerWidth)
+    container.style.width = wSize / 2 + "px"
+    container.style.height = wSize / 2 + "px"
 
 
-  var rects=[[]];
+    var rects = [[]];
 
-  var position = [[new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook")], 
-    [new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook")], 
-    [], 
-    [], 
-    [], 
-    [], 
-    [new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook")], 
+    var position = [[new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook")],
+    [new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook"), new Figure("b", "rook")],
+    [],
+    [],
+    [],
+    [],
+    [new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook")],
     [new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook"), new Figure("w", "rook")]];
 
-  console.log("Window size", wSize)
-  console.log("i elem in Array", position[0][1])
+    console.log("Window size", wSize)
+    console.log("i elem in Array", position[0][1])
 
-  function createRect(size="50px", colour="black") {
-    var el = document.createElement("div")
-    el.style.width = size;
-    el.style.height = size;
-    el.style.background = colour;
-    el.style.float = "left"
-    rects.push(el)
-    return el
-  }
-
-  for (var i=0; i<8;i++) {
-    for (var j=0; j<8; j++) {
-      container.appendChild(createRect(wSize/16+"px", ((i+j)%2==0)?"#a0a0a0":"#eee"))
+    function createRect(size = "50px", colour = "black") {
+        var el = document.createElement("div")
+        el.style.width = size;
+        el.style.height = size;
+        el.style.background = colour;
+        el.style.float = "left"
+        rects.push(el)
+        return el
     }
-  }
 
-  //rects = container.querySelectorAll("div");
-
-  console.log(rects)
-  setFigs(position)
-
-  function setFigs(arr) {
-    var fig;
-
-    console.log(arr)
-
-    for (var i=0; i<8;i++) {
-      for (var j=0; j<8; j++) {
-        debugger;
-        if (arr[i][j]!=undefined) arr[i][j].draw(rects[8*i+j+1])
-      }
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            container.appendChild(createRect(wSize / 16 + "px", ((i + j) % 2 == 0) ? "#a0a0a0" : "#eee"))
+        }
     }
-  }
+
+    //rects = container.querySelectorAll("div");
+
+    console.log(rects)
+    setFigs(position)
+
+    function setFigs(arr) {
+        var fig;
+
+        console.log(arr)
+
+        for (var i = 0; i < 8; i++) {
+            for (var j = 0; j < 8; j++) {
+                debugger;
+                if (arr[i][j] != undefined) arr[i][j].draw(rects[8 * i + j + 1])
+            }
+        }
+    }
 
 
 }
@@ -63,10 +63,8 @@ function makeBoard() {
 //  var queen_w = new Queen("white")
 //  queen_w.validate();
 //}
-
 window.onload = () => {
-  makeBoard()
-  //play()
+    makeBoard()
+    //play()
 };
-
 
